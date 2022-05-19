eb203bb5ae95875c660d6f079415cc7b513403f1
James Metke <james_metke@hotmail.com>
2022-05-18 20:38:32 -0600

    state machine now constrains actor type

--- src/common/decisions/StateMachine.ts
+++ src/common/decisions/StateMachine.ts
@@ -5,6 +5,11 @@ import { AdjList } from "common/graph/AdjacencyList";
 import { DiGraph, DirectedEdge } from "common/graph/Digraph";
 import { DecisionMaker } from "./DecisionMaker";
 
+export interface Stateful<actor_t, result_t> {
+	getState(stateMachine: StateMachine<actor_t & Stateful<actor_t, result_t>, result_t>): State<actor_t, result_t>;
+	setState(state: State<actor_t, result_t>): void;
+}
+
 export class State<actor_t, result_t> {
 	public entry?: DecisionMaker<actor_t, result_t>;
 	public exit?: DecisionMaker<actor_t, result_t>;
@@ -24,20 +29,23 @@ export class Transition<actor_t, result_t> implements DirectedEdge<State<actor_t
 	}
 }
 
-export class StateMachine<actor_t, result_t> implements DecisionMaker<actor_t, result_t>
+export class StateMachine<actor_t extends Stateful<actor_t, result_t>, result_t> implements DecisionMaker<actor_t, result_t>
 {
-	public current: State<actor_t, result_t>;
 	public graph: DiGraph<State<actor_t, result_t>, Transition<actor_t, result_t>>;
+	public initial: State<actor_t, result_t>;
 	constructor(initial: State<actor_t, result_t>, graph?: DiGraph<State<actor_t, result_t>, Transition<actor_t, result_t>>) {
-		this.current = initial;
+		this.initial = initial;
 		this.graph = graph ?? new AdjList<State<actor_t, result_t>, Transition<actor_t, result_t>>();
 		graph ?? this.graph.addVertex(initial);
 	}
 	decide(actor: actor_t): Action<actor_t, result_t> | undefined {
-		const transition = Array.from(this.graph.edgesFrom(this.current))
+		if (!this.graph.contains(actor.getState(this)))
+			actor.setState(this.initial);
+		const current = actor.getState(this);
+		const transition = Array.from(this.graph.edgesFrom(current))
 			.find(transition => transition.condition.evaluate(actor));
-		if (!transition) return this.current.body?.decide(actor);
-		this.current = transition.to;
+		if (!transition) return current.body?.decide(actor);
+		actor.setState(transition.to);
 		return new ActionSequence<actor_t, result_t>()
 			.then(transition.from.body?.decide(actor))
 			.then(transition.logic?.decide(actor))
